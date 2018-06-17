3cb391a57ca19ef26f7332fac18e6e0c5df7b0bd
Lee HanYeong <dev@lhy.kr>
2018-06-17 17:54:28 +0900

    customerid bpchar -> smallint change

--- create_db.sh
+++ create_db.sh
@@ -2,11 +2,11 @@
 
 dropdb northwind
 dropuser northwind_user
+createuser northwind_user
 
-createdb northwind
-psql northwind < northwind.sql
+createdb northwind --owner=northwind_user
+psql northwind --user=northwind_user < northwind.sql
 
-psql template1 -c "create user northwind_user;"
 psql template1 -c "alter user northwind_user password 'thewindisblowing';"
 psql template1 -c "grant all on DATABASE northwind to northwind_user;"
 psql northwind -c "GRANT ALL on ALL tables IN SCHEMA public to northwind_user"
